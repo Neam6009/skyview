52ccd6e0f47393bf81a7f4121713c013dd45a396
neelesh <neeleshnani99@gmail.com>
2024-11-27 23:42:01 +0530

    rechart

--- src/app/components/admin_dashboard/chart/Chart.jsx
+++ src/app/components/admin_dashboard/chart/Chart.jsx
@@ -1,7 +1,7 @@
 "use client"
 import React, { PureComponent } from 'react';
 import classes from './chart.module.css'
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+// import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
 const data = [
   {
@@ -44,7 +44,7 @@ const data = [
 const Chart = () => {
   return (
     <div className={classes.container}>
-      <h2 className={classes.title}>Weekly Recap</h2>
+      {/* <h2 className={classes.title}>Weekly Recap</h2>
       <ResponsiveContainer width="100%" height="90%">
         <LineChart
           width={500}
@@ -64,7 +64,7 @@ const Chart = () => {
           <Line type="monotone" dataKey="visit" stroke="#8884d8" strokeDasharray="5 5" />
           <Line type="monotone" dataKey="click" stroke="#82ca9d" strokeDasharray="3 4 5 2" />
         </LineChart>
-      </ResponsiveContainer>
+      </ResponsiveContainer> */}
     </div>
   )
 }
